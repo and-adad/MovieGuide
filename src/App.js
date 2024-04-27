da0609f2363baea69ec380336bd99b4cec34f4d6
Andrey <Andreyadad@gmail.com>
2024-04-27 11:59:24 +0500

    С тремя статическими данными играюсь с адаптивностью под мобильые устройства - выстраивается в одну колонку, а для широкого вьюпорта три в ряд

--- src/App.js
+++ src/App.js
@@ -11,6 +11,22 @@ const movie1 = {
   "Poster": "N/A"
 }
 
+const movie2 =     {
+  "Title": "Superman, Spiderman or Batman",
+  "Year": "2011",
+  "imdbID": "tt2084949",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
+}
+
+const movie3 =     {
+  "Title": "Italian Spiderman",
+  "Year": "2007",
+  "imdbID": "tt2705436",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
+}
+
 const MovieCard = (props) => {
   return (
     <>
@@ -59,8 +75,22 @@ function App(props) {
             <div>
               <p>{movie1.Year}</p>
             </div>
-          </div>
 
+            <div>
+              <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+            </div>
+
+          </div>
+          <div className='movie'>
+            <div>
+              <img src={movie2.Poster !== 'N/A' ? movie2.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+            </div>
+          </div>
+          <div className='movie'>
+           <div>
+              <img src={movie3.Poster !== 'N/A' ? movie3.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+            </div>
+          </div>
         </div>
     </div>
   );
