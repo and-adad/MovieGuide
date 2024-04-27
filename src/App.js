d0097c62f58e3d0ba086ea7eeea7f45fbcadf76e
Andrey <Andreyadad@gmail.com>
2024-04-27 18:15:53 +0500

    Мелкие правки

--- src/App.js
+++ src/App.js
@@ -4,38 +4,39 @@ import MovieCard from './MovieCard';
 
 const API_URL = 'http://www.omdbapi.com?apikey=17249cd7'
 
-const movie1 = {
-  "Title": "Spiderman",
-  "Year": "1990",
-  "imdbID": "tt0100669",
-  "Type": "movie",
-  "Poster": "N/A"
-}
+// const movie1 = {
+//   "Title": "Spiderman",
+//   "Year": "1990",
+//   "imdbID": "tt0100669",
+//   "Type": "movie",
+//   "Poster": "N/A"
+// }
 
-const movie2 =     {
-  "Title": "Superman, Spiderman or Batman",
-  "Year": "2011",
-  "imdbID": "tt2084949",
-  "Type": "series",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
-}
+// const movie2 =     {
+//   "Title": "Superman, Spiderman or Batman",
+//   "Year": "2011",
+//   "imdbID": "tt2084949",
+//   "Type": "series",
+//   "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
+// }
 
-const movie3 =     {
-  "Title": "Italian Spiderman",
-  "Year": "2007",
-  "imdbID": "tt2705436",
-  "Type": "movie",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
-}
+// const movie3 =     {
+//   "Title": "Italian Spiderman",
+//   "Year": "2007",
+//   "imdbID": "tt2705436",
+//   "Type": "movie",
+//   "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
+// }
 
 const MovieCardMe = (props) => {
   return (
     <>
       <h1>Title: {props.name}</h1>
+      <h2>Year: 1996</h2>
     </>
   )
 }
-
+// если мы больше одной строки то фрагмент использовать обязательно <></>
 
 function App(props) {
   const [movies, setMovies] = useState([]);
@@ -67,11 +68,11 @@ function App(props) {
         <h1>{counter}</h1>
         <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
 
-        <MovieCardMe name={'Spiderman'} />
-        <MovieCardMe name={'Superman'} />
-        <MovieCardMe name={'Bananaman'} />
-        <MovieCardMe name={'Batman'} />
-        <MovieCardMe name={'Man'} />
+        <MovieCardMe name='Spiderman' />
+        <MovieCardMe name='Bananaman' />
+        <MovieCardMe name='Superman' />
+        <MovieCardMe name='Batman' />
+        <MovieCardMe name='Man' />
 
         {movies?.length > 0 ? (
             <div className='container'>
