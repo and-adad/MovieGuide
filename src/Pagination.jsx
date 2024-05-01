89ad60142b840ec657315c00c569be617564ebac
Andrey <Andreyadad@gmail.com>
2024-05-01 17:11:52 +0500

    Заменил иконку кнопки вперед на стрелочку как в задании

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
 import Pagination from '@mui/material/Pagination';
+import PaginationItem from '@mui/material/PaginationItem';
 import Stack from '@mui/material/Stack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
 
 export default function PaginationRounded() {
   return (
     <Stack spacing={2}>
-      <Pagination count={5} hidePrevButton shape="rounded" size="large" />
+      <Pagination
+        count={5}
+        hidePrevButton
+        shape="rounded"
+        size="large"
+        renderItem={(item) => (
+            <PaginationItem
+              slots={{ next: ArrowForwardIcon }}
+              {...item}
+            />
+          )} />
     </Stack>
   );
 }
