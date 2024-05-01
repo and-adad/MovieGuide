5136c131d1c64c52a308391b0c4afe8802ec3b48
Andrey <Andreyadad@gmail.com>
2024-05-01 16:37:52 +0500

    Удалил из примера дублирующую пагинацию

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -6,7 +6,6 @@ export default function PaginationRounded() {
   return (
     <Stack spacing={2}>
       <Pagination count={10} shape="rounded" />
-      <Pagination count={10} variant="outlined" shape="rounded" />
     </Stack>
   );
 }
